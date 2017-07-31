c623c53e58fbe616bb758c4e56de0fe442143f1b
Amr El Bakry <amr.bakry@yallawain.com>
2017-07-31 17:04:25 +0200

    Add search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
  var React = require('react');
  var TodoList = require('TodoList');
  var AddTodo = require('AddTodo');
+ var TodoSearch = require('TodoSearch');
 
  var TodoApp = React.createClass({
    getInitialState: function() {
@@ -23,8 +24,16 @@
            text: 'rule the world'
          }
 
-       ]
+       ],
+       showCompleted: false,
+       searchText: ''
      }
+   },
+   handleSearch: function(showCompleted, searchText) {
+     this.setState({
+       showCompleted: showCompleted,
+       searchText: searchText.toLowerCase()
+     });
    },
    handleAddTodo: function(text) {
      alert('new todo: ' + text);
@@ -34,6 +43,7 @@
      return(
        <div className='row'>
          <div className='column medium-6 small-4'>
+           <TodoSearch onSearch={this.handleSearch}/>
            <TodoList todos={todos}/>
            <AddTodo onAddTodo={this.handleAddTodo}/>
          </div>
