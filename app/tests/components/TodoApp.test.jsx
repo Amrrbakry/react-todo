abfcd6f1e9c8bcb34d574fb009a0cd034f1c3809
Amr El Bakry <amr.bakry@yallawain.com>
2017-08-01 17:24:47 +0200

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,10 +19,17 @@ describe('TodoApp', () => {
     todoApp.handleAddTodo(todoText);
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
+    expect(todoApp.state.todos[0].createdAt).toBeA('number');
   });
 
   it('should toggle completed value when hanldeToggle called', () => {
-    var todoData = {id: 11, text: 'testtt', completed: false};
+    var todoData = {
+      id: 11,
+      text: 'testtt',
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
+    };
 
     var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos: [todoData]});
@@ -30,5 +37,22 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(todoData.id);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(todoApp.state.todos[0].completedAt).toBeA('number');
+  });
+
+  it('should clear completedAt value when toggled from true to false', () => {
+    var todoData = {
+      id: 11,
+      text: 'testtt',
+      completed: false,
+      createdAt: 0,
+      completedAt: undefined
+    };
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
   });
 });
